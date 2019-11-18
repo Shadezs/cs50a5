d0d60d449a5af3ff5aeba237b349b2c9615fc6c0
BuildTools <carlosdakk@yahoo.com>
2019-11-17 18:07:43 -0800

    added new fuction, needs more work
    Signed-off-by: BuildTools <carlosdakk@yahoo.com>

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 int main() {
     printf("Hello, World!\n");
     int user_inputdata[20];
-    int* user_data;
+    int* user_data =user_data[0];
     int user_input=0;
     printf_s("please enter 20 interger \n");
+    for (int i = 0; i <20 ; i++) {
+        scanf_s("d%",&user_input);
+        user_data=user_input;
 
-    scanf("d%",&user_inputdata[0]);
-
-
-
+    }
     for (int j = 0; j < 20; j++) {
         printf_s("the Numbers you enter are: d%",user_inputdata[j]);
     }
